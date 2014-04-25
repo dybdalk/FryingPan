823fffe798f1cf43763f0bdcf66eec1934c0bc99
nmurray53 <n.murray53@yahoo.com>
2014-04-25 12:04:44 -0700

    Update cart.php

--- serverCopy/cart.php
+++ serverCopy/cart.php
@@ -10,15 +10,17 @@ $item3[0] = 0;
 
 
 function addi1($item1){
-	for($i = $item1.count()-1;  $i <= 1000; $item1+){
+	for($i = $item1.count()-1;  $i <= 1000; $item1+)
 
 		echo $item1; }
 }
 
 function addi2($item2){
-	for($i = $item2.count()-1;  $i <= 1000; $item2++){
+	for($i = $item2.count()-1;  $i <= 1000; $item2++)
 		echo $item2; }
 
 function addi3($item3){
-	for($i = $item3.count()-1;  $i <= 1000; $item3++){
+	for($i = $item3.count()-1;  $i <= 1000; $item3++)
 		echo $item3; }
+
+?>
