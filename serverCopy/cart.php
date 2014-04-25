bae4ac4469ac0458f61f30baba39e83e3b43b50b
nmurray53 <n.murray53@yahoo.com>
2014-04-25 12:03:56 -0700

    Create cart.php

---
+++ serverCopy/cart.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+$item1 = array();
+$item1[0] = 0;
+$item2 = array();
+$item2[0] = 0;
+$item3 = array();
+$item3[0] = 0;
+
+
+function addi1($item1){
+	for($i = $item1.count()-1;  $i <= 1000; $item1+){
+
+		echo $item1; }
+}
+
+function addi2($item2){
+	for($i = $item2.count()-1;  $i <= 1000; $item2++){
+		echo $item2; }
+
+function addi3($item3){
+	for($i = $item3.count()-1;  $i <= 1000; $item3++){
+		echo $item3; }
